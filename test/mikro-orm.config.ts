ffc07ecb984023e93489db18f4c69960e2552535
Ray Smets <rayjsmets@gmail.com>
2022-12-14 12:25:43 -0800

    [types]

--- test/mikro-orm.config.ts
+++ test/mikro-orm.config.ts
@@ -1,9 +1,10 @@
 import { Options } from '@mikro-orm/core';
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
 import { BaseEntity } from './entities/BaseEntity';
 import { Book } from './entities/Book';
 
-export const config: Options = {
+export const config: Options<PostgreSqlDriver> = {
   type: 'postgresql',
   dbName: 'feathers_mikro_orm_test',
   host: 'localhost',
--- test/mikro-orm.ts
+++ test/mikro-orm.ts
@@ -1,4 +1,5 @@
 import { MikroORM } from '@mikro-orm/core';
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 
 import type { Application } from './declarations';
 import { config } from './mikro-orm.config';
@@ -10,7 +11,7 @@ declare module './declarations' {
 }
 
 export const mikroOrm = async (app: Application): Promise<void> => {
-  const orm = await MikroORM.init(config);
+  const orm = await MikroORM.init<PostgreSqlDriver>(config);
 
   app.set('orm', orm);
 };
